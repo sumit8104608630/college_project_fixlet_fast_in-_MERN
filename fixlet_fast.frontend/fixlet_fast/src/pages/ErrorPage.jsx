dd581e737d44138cc766bad76d827455c282e4c4
sumit sharma <114969811+sumit8104608630@users.noreply.github.com>
2024-12-07 19:18:59 +0530

    started frontrnd for the fixlet_fast application

---
+++ fixlet_fast.frontend/fixlet_fast/src/pages/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ErrorPage() {
+  return (
+    <div className='flex flex-col gap-8 justify-center  items-center' style={{height:"100vh"}}>
+    <h1 className='text-6xl text-gray-700 border-b-2 py-5'>404 Error</h1>
+    <li className='list-none text-2xl font-semibold text-gray-700'><NavLink className={"hover:text-blue-700"} to={"/"}>Go Back</NavLink></li>
+  </div>  
+  )
+}
+
+export default ErrorPage
